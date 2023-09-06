4895bdcc44e0b77399dbb8b529e53ce24ffa0472
bohsav22-private <143456933+bohsav22-private@users.noreply.github.com>
2023-09-06 10:02:33 +0300

    refactoring Program.cs, adding ApplicationInstaller

---
+++ LanguageStudyAPI/Installers/ApplicationInstaller.cs
@@ -0,0 +1,49 @@
+﻿using LanguageStudyAPI.Authentication;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace LingvoInfoAPI.Installers
+{
+    public static class ApplicationInstaller
+    {
+        public static void ConfigureLingueeHttpClient(WebApplicationBuilder builder)
+        {
+            builder.Services.AddHttpClient("LingueeApi", client =>
+            {
+                client.BaseAddress = new Uri(builder.Configuration["LingueeApi:BaseUrl"]
+                    ?? throw new ArgumentNullException("Linguee API Base URI wasn't found in configuration settings"));
+            }).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+            {
+                AllowAutoRedirect = true,
+                MaxAutomaticRedirections = 5
+            });
+        }
+        public static void ConfigureLingvoHttpClient(WebApplicationBuilder builder)
+        {
+            builder.Services.AddHttpClient("LingvoApi", client =>
+            {
+                client.BaseAddress = new Uri(builder.Configuration["LingvoApi:BaseUrl"]
+                    ?? throw new InvalidOperationException("BaseUrl for Lingvo API is not configured."));
+            })
+            .AddHttpMessageHandler<LingvoApiAuthenticationHandler>();
+        }
+        public static IApiAuthenticationService CreateLingvoApiAuthenticationService(IServiceProvider serviceProvider, IConfiguration configuration)
+        {
+            var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+            var memoryCache = serviceProvider.GetRequiredService<IMemoryCache>();
+            var httpClient = httpClientFactory.CreateClient();
+
+            string baseUrl = configuration["LingvoApi:BaseUrl"]
+                ?? throw new InvalidOperationException("BaseUrl for Lingvo API is not configured.");
+            httpClient.BaseAddress = new Uri(baseUrl);
+
+            string apiKey = configuration["LingvoApi:ApiKey"]
+                ?? throw new InvalidOperationException("ApiKey for Lingvo API is not configured.");
+
+            string tokenExpirationInMinutesString = configuration["LingvoApi:TokenExpirationInMinutes"]
+                ?? throw new InvalidOperationException("Token Expiration for Lingvo API is not configured.");
+            int tokenExpirationInMinutes = int.Parse(tokenExpirationInMinutesString);
+
+            return new LingvoApiAuthenticationService(httpClient, memoryCache, apiKey, tokenExpirationInMinutes);
+        }
+    }
+}
--- LanguageStudyAPI/Program.cs
+++ LanguageStudyAPI/Program.cs
@@ -2,79 +2,47 @@ using LanguageStudyAPI.Authentication;
 using LanguageStudyAPI.Mappers;
 using LanguageStudyAPI.Services;
 using LingvoInfoAPI.Clients;
+using LingvoInfoAPI.Installers;
 using LingvoInfoAPI.Services;
 using Mapster;
 using Microsoft.Extensions.Caching.Memory;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Controllers and API documentation
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Cache
 builder.Services.AddMemoryCache();
 
+// Mappers
 MapsterConfig.Configure();
-//var config = TypeAdapterConfig.GlobalSettings;
 
-//var config = new TypeAdapterConfig();
-//config.NewConfig<Student, StudentDTO>()
-//    .Map(dest => dest.Name, src => src.FullName);
-
-builder.Services.AddScoped<ILingvoInfoService, LingvoInfoService>();
-
+// API Clients
 builder.Services.AddScoped<GoogleTranslationAPIClient>();
 builder.Services.AddScoped<LingueeApiClient>();
 builder.Services.AddScoped<LingvoApiClient>();
 
-
+// Services
+builder.Services.AddScoped<ILingvoInfoService, LingvoInfoService>();
 builder.Services.AddScoped<GoogleTranslationAPIService>();
 builder.Services.AddScoped<LingueeApiService>();
-builder.Services.AddHttpClient("LingueeApi", client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["LingueeApi:BaseUrl"]
-        ?? throw new ArgumentNullException("Linguee API Base URI wasn't found in configuration settings"));
-}).ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
-{
-    AllowAutoRedirect = true,
-    MaxAutomaticRedirections = 5
-});
-
 builder.Services.AddScoped<ILingvoApiService, LingvoApiService>();
 
+// Http Clients Configuration
+ApplicationInstaller.ConfigureLingueeHttpClient(builder);
+ApplicationInstaller.ConfigureLingvoHttpClient(builder);
+
+// Authentication
 builder.Services.AddScoped<LingvoApiAuthenticationHandler>();
-
-builder.Services.AddHttpClient("LingvoApi", client =>
-{
-    client.BaseAddress = new Uri(builder.Configuration["LingvoApi:BaseUrl"]
-        ?? throw new InvalidOperationException("BaseUrl for Lingvo API is not configured."));
-})
-.AddHttpMessageHandler<LingvoApiAuthenticationHandler>();
-
-builder.Services.AddTransient<IApiAuthenticationService>(serviceProvider =>
-{
-    var httpClientFactory = serviceProvider.GetRequiredService<IHttpClientFactory>();
-    var memoryCache = serviceProvider.GetRequiredService<IMemoryCache>();
-    var httpClient = httpClientFactory.CreateClient();
-
-    string baseUrl = builder.Configuration["LingvoApi:BaseUrl"]
-        ?? throw new InvalidOperationException("BaseUrl for Lingvo API is not configured.");
-    httpClient.BaseAddress = new Uri(baseUrl);
-
-    string apiKey = builder.Configuration["LingvoApi:ApiKey"]
-        ?? throw new InvalidOperationException("ApiKey for Lingvo API is not configured.");
-
-    string tokenExpirationInMinutesString = builder.Configuration["LingvoApi:TokenExpirationInMinutes"]
-        ?? throw new InvalidOperationException("Token Expiration for Lingvo API is not configured.");
-    int tokenExpirationInMinutes = int.Parse(tokenExpirationInMinutesString);
-
-    return new LingvoApiAuthenticationService(httpClient, memoryCache, apiKey, tokenExpirationInMinutes);
-});
+builder.Services.AddTransient<IApiAuthenticationService>(sp =>
+    ApplicationInstaller.CreateLingvoApiAuthenticationService(sp, builder.Configuration));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Development-specific configurations
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -82,11 +50,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
 
 public partial class Program { }
